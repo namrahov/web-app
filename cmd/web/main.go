f1df15ee18d42846874e42dac8f6a67acc5670a1
namrahov <nurlan.amrahov@pashabank.az>
2021-08-04 12:57:46 +0400

    TooHotToHoot

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,18 +2,33 @@ package main
 
 import (
 	"fmt"
+	"github.com/alexedwards/scs/v2"
 	"github.com/namrahov/web-app/pkg/config"
 	"github.com/namrahov/web-app/pkg/handlers"
 	"github.com/namrahov/web-app/pkg/render"
 	"log"
 	"net/http"
+	"time"
 )
 
 const portNumber = ":8080"
 
+var app config.AppConfig
+var session *scs.SessionManager
+
 // main is the main function
 func main() {
-	var app config.AppConfig
+	// change this to true when in production
+	app.InProduction = false
+
+	// set up the session
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
 
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
